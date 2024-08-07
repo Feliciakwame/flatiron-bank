1f1057c31ef2ba474894fcae3d49e966fecbc381
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-08-07 19:10:44 +0300

    Creation of banklist and data fetching

--- src/components/BankList.jsx
+++ src/components/BankList.jsx
@@ -1,27 +1,44 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 function BankList() {
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3000/transactions`)
+      .then((res) => res.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(transactions);
   return (
-    <div>
-      <table className="BankList">
+    <table className="BankList">
+      <thead>
         <tr>
-          <th>
-            <h2>
-              <strong>Date</strong>
-            </h2>
+          <th className="DateList">
+            <h2>Date</h2>
           </th>
-          <th>
+          <th className="DescriptionList">
             <h2>Description</h2>
           </th>
-          <th>
+          <th className="CategoryList">
             <h2>Category</h2>
           </th>
-          <th>
+          <th className="AmountList">
             <h2>Amount</h2>
           </th>
         </tr>
-      </table>
-    </div>
+      </thead>
+      <tbody>
+        {transactions.map((data, id) => (
+          <tr key={id}>
+            <td>{data.date}</td>
+            <td>{data.description}</td>
+            <td>{data.category}</td>
+            <td>{data.amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
